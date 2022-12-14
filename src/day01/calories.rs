0a1e75028ed6c20ae268308c7fee5061c6504dba
Brian Hardy <lyricsboy@users.noreply.github.com>
2022-12-14 09:28:12 -0500

    Day 1 part 2

--- src/day01/calories.rs
+++ src/day01/calories.rs
@@ -9,6 +9,16 @@ pub(crate) fn max_calories<T:BufRead>(iterator: &mut Lines<T>) -> Option<i32> {
     return calorie_counts.iter().max().cloned();
 }
 
+pub(crate) fn top_three_total<T:BufRead>(iterator: &mut Lines<T>) -> i32 {
+    let mut calorie_counts: Vec<i32> = Vec::new();
+    while let Some(calorie_count) = count_calories(iterator) {
+        calorie_counts.push(calorie_count);
+    }
+    calorie_counts.sort_by(|a, b| b.cmp(a));
+    let sum: i32 = calorie_counts[0..3].iter().sum();
+    return sum;
+}
+
 fn count_calories<T:BufRead>(iterator: &mut Lines<T>) -> Option<i32> {
     iterator.map_while(|line| {
         line.unwrap().parse::<i32>().ok()
@@ -50,5 +60,13 @@ mod tests {
         let buf = BufReader::new(input.as_bytes());
         let max_calories = max_calories(&mut buf.lines());
         assert_eq!(max_calories, Some(24000));
+    }
+
+    #[test]
+    fn top_three_total_with_sample_input() {
+        let input = SAMPLE_INPUT.to_string(); 
+        let buf = BufReader::new(input.as_bytes());
+        let top_three_calories = top_three_total(&mut buf.lines());
+        assert_eq!(top_three_calories, 45000);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ mod day01;
 
 fn main() {
     let stdin = io::stdin();
-    let max_calories = day01::calories::max_calories(&mut stdin.lock().lines());
-    println!("Max Calories: {:?}", max_calories);
+    let top_three_total = day01::calories::top_three_total(&mut stdin.lock().lines());
+    println!("Top 3 total: {:?}", top_three_total);
 }
