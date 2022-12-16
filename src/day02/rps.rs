664f343576cfde5aa1faa6429a59a9cf8d1c4bc2
Brian Hardy <lyricsboy@users.noreply.github.com>
2022-12-15 21:15:09 -0500

    Day 2 part 2

--- src/day02/rps.rs
+++ src/day02/rps.rs
@@ -1,9 +1,16 @@
 use std::io::{BufRead, Lines};
 
+#[derive(Copy, Clone)]
 enum RPSMove {
     Rock,
     Paper,
     Scissors
+}
+
+enum RPSResult {
+    Win,
+    Lose,
+    Draw
 }
 
 fn decode_move(c: char) -> Option<RPSMove> {
@@ -11,10 +18,32 @@ fn decode_move(c: char) -> Option<RPSMove> {
         'A' => Some(RPSMove::Rock),
         'B' => Some(RPSMove::Paper),
         'C' => Some(RPSMove::Scissors),
-        'X' => Some(RPSMove::Rock),
-        'Y' => Some(RPSMove::Paper),
-        'Z' => Some(RPSMove::Scissors),
         _ => None
+    }
+}
+
+fn decode_result(c: char) -> Option<RPSResult> {
+    match c {
+        'X' => Some(RPSResult::Lose),
+        'Y' => Some(RPSResult::Draw),
+        'Z' => Some(RPSResult::Win),
+        _ => None
+    }
+}
+
+fn your_move_for(their_move: RPSMove, result: RPSResult) -> RPSMove {
+    match result {
+        RPSResult::Win =>  match their_move {
+            RPSMove::Rock => RPSMove::Paper,
+            RPSMove::Paper => RPSMove::Scissors,
+            RPSMove::Scissors => RPSMove::Rock,
+        },
+        RPSResult::Lose => match their_move {
+            RPSMove::Rock => RPSMove::Scissors,
+            RPSMove::Paper => RPSMove::Rock,
+            RPSMove::Scissors => RPSMove::Paper,
+        },
+        RPSResult::Draw => their_move,
     }
 }
 
@@ -38,13 +67,18 @@ fn rps_round_score(their_move: RPSMove, your_move: RPSMove) -> i32 {
 pub(crate) fn rps_score<T:BufRead>(iterator: &mut Lines<T>) -> i32 {
     return iterator.fold(0, |score: i32, line: Result<String, std::io::Error>| -> i32 {
         let line_str = line.unwrap();
-        let mut moves = line_str.split_whitespace().map(|encoded_move| {
-            let encoded_char = encoded_move.chars().nth(0).unwrap();
-            decode_move(encoded_char).unwrap()
-        });
-        let their_move = moves.nth(0);
-        let your_move = moves.nth(0);
-        score + rps_round_score(their_move.unwrap(), your_move.unwrap())
+        let mut game_strategy = line_str.split_whitespace().map(|encoded_move|
+            encoded_move.chars().nth(0).unwrap()
+        );
+        let their_move = game_strategy.nth(0)
+            .map(|encoded_move| decode_move(encoded_move) )
+            .flatten().expect("Could not decode their move");
+        let your_move = game_strategy.nth(0)
+            .map(|encoded_result| decode_result(encoded_result) ).flatten()
+            .map(|result| your_move_for(their_move, result))
+            .expect("Could not determine your move");
+
+        score + rps_round_score(their_move, your_move)
     });
 }
 
@@ -63,6 +97,6 @@ C Z";
         let input = SAMPLE_INPUT.to_string(); 
         let buf = BufReader::new(input.as_bytes());
         let score = rps_score(&mut buf.lines());
-        assert_eq!(score, 15);
+        assert_eq!(score, 12);
     }
 }
