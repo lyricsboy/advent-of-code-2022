d1c3fd230d126b48965f3ce5d54142e9b9310140
Brian Hardy <lyricsboy@users.noreply.github.com>
2023-01-26 21:20:36 -0500

    Day 3 Part 2

--- src/day03/rucksack.rs
+++ src/day03/rucksack.rs
@@ -1,9 +1,17 @@
-use std::{io::{BufRead, Lines}, collections::HashSet};
+use std::{io::{BufRead, Lines, Error}, collections::HashSet};
+use itertools::Itertools;
 
 fn find_common_items<T:BufRead>(iterator: &mut Lines<T>) -> Vec<char> {
     iterator.flat_map(|line| 
         find_common_item(line.unwrap())
     ).collect()
+}
+
+fn find_common_item_between_rucksacks<T:Iterator<Item = Result<String, Error>>>(iterator: &mut T) -> Option<char> {
+    iterator.map(|line| HashSet::<_>::from_iter(line.unwrap().chars()))
+        .reduce(|intersection, set| intersection.intersection(&set).cloned().collect())
+        .map(|maybe_set| maybe_set.into_iter().nth(0))
+        .flatten()
 }
 
 fn find_common_item(rucksack_contents: String) -> Option<char> {
@@ -26,6 +34,13 @@ fn priority_sum(items: Vec<char>) -> u32 {
 
 pub(crate) fn priority_sum_from_lines<T:BufRead>(lines: &mut Lines<T>) -> u32 {
     priority_sum(find_common_items(lines))
+}
+
+pub(crate) fn priority_sum_of_common_items<T:Iterator<Item = Result<String, Error>>>(iterator: &mut T) -> u32 {
+    let common_items = iterator.chunks(3).into_iter()
+        .flat_map(|mut chunk| find_common_item_between_rucksacks(&mut chunk))
+        .collect();
+    return priority_sum(common_items);
 }
 
 #[cfg(test)]
@@ -52,5 +67,27 @@ CrZsJsPPZsGzwwsLwLmpwMDw";
     #[test]
     fn priority_sum_with_sample_input() {
         assert_eq!(priority_sum(['p', 'L', 'P', 'v', 't', 's'].to_vec()), 157)
+    }
+
+    #[test]
+    fn find_common_item_between_rucksacks_with_sample_input() {
+        let input = SAMPLE_INPUT.to_string(); 
+        let mut buf = BufReader::new(input.as_bytes());
+        let mut first3 = buf.lines().take(3);
+        let mut common_item = find_common_item_between_rucksacks(&mut first3);
+        assert_eq!(common_item, Some('r'));
+        // recreating the buf to avoid complaints about move/copy semantics
+        buf = BufReader::new(input.as_bytes());
+        let mut second3 = buf.lines().skip(3).take(3);
+        common_item = find_common_item_between_rucksacks(&mut second3);
+        assert_eq!(common_item, Some('Z'));
+    }
+
+    #[test]
+    fn find_priority_sum_of_common_items() {
+        let input = SAMPLE_INPUT.to_string(); 
+        let mut buf = BufReader::new(input.as_bytes());
+        let sum = priority_sum_of_common_items(&mut buf.lines());
+        assert_eq!(sum, 70);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,6 @@ mod day03;
 
 fn main() {
     let stdin = io::stdin();
-    let priority_sum = day03::rucksack::priority_sum_from_lines(&mut stdin.lock().lines());
-    println!("Priority Sum: {:?}", priority_sum);
+    let common_item_sum = day03::rucksack::priority_sum_of_common_items(&mut stdin.lock().lines());
+    println!("Common item sum: {:?}", common_item_sum);
 }
