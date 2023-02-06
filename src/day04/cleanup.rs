d22547b99dc161ba446397e9ab9b9b5de18177f3
Brian Hardy <lyricsboy@users.noreply.github.com>
2023-02-05 21:55:52 -0500

    Day 4 part 1

---
+++ src/day04/cleanup.rs
@@ -0,0 +1,59 @@
+use std::{io::{BufRead, Lines, Error}, ops::{Range, RangeBounds}};
+
+pub(crate) fn count_overlapping_ranges<T:Iterator<Item = Result<String, Error>>>(iterator: &mut T) -> usize {
+    let bools = iterator.filter_map(|line| line.ok().and_then(is_overlapping_ranges));
+    return bools.filter(|b| *b).count()
+}
+
+fn is_overlapping_ranges(two_range_line: String) -> Option<bool> {
+    let ranges: Vec<Range<u32>> = two_range_line
+        .split(',')
+        .map(string_to_range)
+        .flatten()
+        .collect();
+    if ranges.len() == 2 {
+        return Some(ranges_overlap(&ranges[0], &ranges[1]))
+    } else {
+        return None
+    }
+}
+
+fn ranges_overlap(range1: &Range<u32>, range2: &Range<u32>) -> bool {
+    (range1.contains(&range2.start) && range1.contains(&(range2.end - 1))) ||
+    (range2.contains(&range1.start) && range2.contains(&(range1.end - 1)))
+}
+
+fn string_to_range(string_range: &str) -> Option<Range<u32>> {
+    let ints: Vec<u32> = string_range
+        .split('-')
+        .map(|n| n.parse::<u32>().ok())
+        .flatten()
+        .collect();
+    if ints.len() == 2 {
+        return Some(ints[0]..ints[1]+1)
+    } else {
+        return  None
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::io::BufReader;
+
+    use super::*;
+
+    const SAMPLE_INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
+    #[test]
+    fn find_common_items_with_sample_input() {
+        let input = SAMPLE_INPUT.to_string(); 
+        let buf = BufReader::new(input.as_bytes());
+        let overlapping_range_count = count_overlapping_ranges(&mut buf.lines());
+        assert_eq!(overlapping_range_count, 2);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,10 @@ use std::io::{self, BufRead};
 mod day01;
 mod day02;
 mod day03;
+mod day04;
 
 fn main() {
     let stdin = io::stdin();
-    let common_item_sum = day03::rucksack::priority_sum_of_common_items(&mut stdin.lock().lines());
-    println!("Common item sum: {:?}", common_item_sum);
+    let overlapping_range_count = day04::cleanup::count_overlapping_ranges(&mut stdin.lock().lines());
+    println!("Overlapping range count: {:?}", overlapping_range_count);
 }
